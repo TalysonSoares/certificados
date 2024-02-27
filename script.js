709618c0e2c35d2d1136801f1047d95240b2ac95
TalysonSoares <talysonsoares40@gmail.com>
2024-02-27 19:11:05 -0300

    voa meu filho

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('dados.xml')
-      .then(response => response.text())
-      .then(data => {
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(data, 'application/xml');
-        const pessoas = xmlDoc.querySelectorAll('pessoa');
-  
-        // Função recursiva para gerar certificados de forma assíncrona
-        function gerarCertificado(index) {
-          if (index < pessoas.length) {
-            const pessoa = pessoas[index];
-            const nome = pessoa.querySelector('nome').textContent;
-            const cpf = pessoa.querySelector('cpf').textContent;
-  
-            // Atualiza o conteúdo da tela com os dados da pessoa
-            document.getElementById('nome').textContent = nome;
-            document.getElementById('cpf').textContent = cpf;
-  
-            //Gera e salva o pdf
-            const element = document.getElementById('certificado');
-            html2pdf(element, { margin: 20, filename: `${nome}_certificado.pdf` })
-              .then(() => {
-                //Passa para a proxima pessoa
-                gerarCertificado(index + 1);
-              });
-          }
+  fetch('dados.xml')
+    .then(response => response.text())
+    .then(data => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, 'application/xml');
+      const pessoas = xmlDoc.querySelectorAll('pessoa');
+
+      
+      function gerarCertificado(index) {
+        if (index < pessoas.length) {
+          const pessoa = pessoas[index];
+          const nome = pessoa.querySelector('nome').textContent;
+          const cpf = pessoa.querySelector('cpf').textContent;
+          const serie = pessoa.querySelector('serie').textContent;
+
+          document.getElementById('nome').textContent = nome;
+          document.getElementById('cpf').textContent = cpf;
+          document.getElementById('serie').textContent = serie;
+          
+          const element = document.getElementById('certificado');
+          html2pdf(element, { margin: 0, filename: `${nome}_certificado.pdf`,})
+            .then(() => {
+              gerarCertificado(index + 1);
+            });
         }
-  
-        //Da inicio ao processo
-        gerarCertificado(0);
-      });
-  });
-  
+      }
+
+      
+      gerarCertificado(0);
+    });
+});
